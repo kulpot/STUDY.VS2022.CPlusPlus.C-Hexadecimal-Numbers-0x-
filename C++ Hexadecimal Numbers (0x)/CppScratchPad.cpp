40633584915b142487c4b919e3fb1ef60e5f5cd8
Kulpot <kulpotwahu@gmail.com>
2024-01-14 03:56:58 +0800

    ...

---
+++ C++ Hexadecimal Numbers (0x)/CppScratchPad.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+// --------------------- C++ Hexadecimal Numbers (0x) ----------------------------------------
+//ref link:https://www.youtube.com/watch?v=XcGIE3LOwLY&list=PLRwVmtr-pp043Ah3GxAqNyoMGyo3_LDNh
+
+void main()
+{
+
+}
